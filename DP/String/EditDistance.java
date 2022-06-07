ad9b40ece8516f0d8ed42093c63bf2089f30ac45
The Coder <34416604+prashantAJ@users.noreply.github.com>
2022-06-07 21:25:02 +0530

    Using both Memoization & Tabulation

--- DP/String/EditDistance.java
+++ DP/String/EditDistance.java
@@ -23,5 +23,28 @@ class Solution {
             replace=1+findMinOperations(i-1, j-1, s1, s2, dp);
         }
         return dp[i][j]=Math.min(insert, Math.min(remove, replace));
+        
+        //Tabulation
+        /*int[][] dp=new int[s1.length()+1][s2.length()+1];
+        for(int j=0;j<=s2.length();j++)
+            dp[0][j]=j;
+        for(int i=0;i<=s1.length();i++)
+            dp[i][0]=i;
+        for(int i=1; i<=s1.length();i++){
+            for(int j=1;j<=s2.length();j++){
+                int insert=0, remove=0, replace=0;
+                if(s1.charAt(i-1)==s2.charAt(j-1))
+                    dp[i][j]=dp[i-1][j-1];
+                else{
+                    insert=1+dp[i][j-1];
+                    remove=1+dp[i-1][j];
+                    replace=1+dp[i-1][j-1];
+                
+               dp[i][j]=Math.min(insert, Math.min(remove, replace)); 
+                }
+            }
+        }
+        return dp[s1.length()][s2.length()];
+        */
     }
 }
